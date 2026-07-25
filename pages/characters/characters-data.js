ab0717f957c5422acd74b0f29315b401bb78c671
Federras <105475542+Reselia@users.noreply.github.com>
2026-07-25 18:51:11 +0300

    Update characters-data.js

--- pages/characters/characters-data.js
+++ pages/characters/characters-data.js
@@ -17,4 +17,20 @@ const characters = [
 
     }
 
+    {
+    id: "FRA-C-002",
+
+    name: "КОНСТАНЦИЯ ГРЕЙЛ",
+
+    position: "Гражданское лицо",
+
+    affiliation: "Республика Эрис",
+
+    photo: "../media/images/characters/constancia.png",
+
+    flag: "../media/images/federation_flat_v4_no_folds.png",
+
+    dossier: "characters/constancia.html"
+}
+
 ];
