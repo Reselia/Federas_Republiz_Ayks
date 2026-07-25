3ac7bfc67c51eb1582bf0e27c0287e8386f1bd23
Federras <105475542+Reselia@users.noreply.github.com>
2026-07-25 06:33:11 +0300

    Create characters-data.js

---
+++ pages/characters/characters-data.js
@@ -0,0 +1,20 @@
+const characters = [
+
+    {
+        id: "FRA-C-001",
+
+        name: "РЕСЕЛИЯ РЭЙ КРИСТАЛИЯ",
+
+        position: "Канцлер",
+
+        affiliation: "Федерация Республик Аякса",
+
+        photo: "../../media/images/characters/reselia.png",
+
+        flag: "../../media/images/federation_flat_v4_no_folds.png",
+
+        profile: "reselia.html"
+
+    }
+
+];
