8b5610040af908078bae5dfbe4cb3c1aefde2589
Federras <105475542+Reselia@users.noreply.github.com>
2026-08-10 20:42:47 +0300

    Create worlds-data.js

---
+++ pages/worlds/worlds-data.js
@@ -0,0 +1,19 @@
+const worlds = [
+
+    {
+        id: "WS-001",
+
+        name: "Эрис",
+
+        flag: "",
+
+        distance: "—",
+
+        type: "Республика",
+
+        lvs: "LVS-1",
+
+        profile: ""
+    }
+
+];
