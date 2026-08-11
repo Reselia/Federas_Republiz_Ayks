10c35b51c3da939c44c19ef491b37ab279c240de
Federras <105475542+Reselia@users.noreply.github.com>
2026-08-11 13:45:16 +0300

    Create persons-data.js

---
+++ pages/interpol/persons-data.js
@@ -0,0 +1,30 @@
+const persons = [
+
+    {
+        id: "692654538200449076",
+
+        name: "Мрамор Итальянский",
+
+        username: "_mramoritalyanskiy",
+
+        photo: "",
+
+        labels: [
+            "ПРЕДАТЕЛЬ"
+        ],
+
+        status: "АКТИВНА",
+
+        dateAdded: "05.10.2025",
+
+        dateRemoved: null,
+
+        term: "БЕССРОЧНО",
+
+        reason: "РЕЗОЛЮЦИЯ № 1211 SHV-А",
+
+        document: ""
+
+    }
+
+];
